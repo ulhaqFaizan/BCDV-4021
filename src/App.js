dc2ad996adc6593e198427ebaa254cbb85b396fa
Faizan ul haq <79333547+ulhaqFaizan@users.noreply.github.com>
2023-10-04 16:38:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeter from "./Greeter";
+
+function App() {
+return (
+  <div>
+    <h1>Hello, React!</h1>
+    <Greeter/>
+  </div>
+);
+}
+
+export default App;
